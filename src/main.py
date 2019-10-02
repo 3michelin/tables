69e1e1418af42fa44defd0dd3cd817b4a15d47cc
3michelin <remingtonskim@gmail.com>
2019-10-02 11:28:30 -0400

    Statement scrubbing fix

--- src/main.py
+++ src/main.py
@@ -3,6 +3,12 @@ import savetable
 
 nameList = tables.getVarName()
 statement = tables.getStatement()
+flag = tables.checkStatement(nameList, statement)
+
+while not flag:
+    statement = tables.getStatement()
+    flag = tables.checkStatement(nameList, statement)
+
 table = tables.getTable(nameList, statement)
 display = tables.formatTable(nameList, statement, table)
 
